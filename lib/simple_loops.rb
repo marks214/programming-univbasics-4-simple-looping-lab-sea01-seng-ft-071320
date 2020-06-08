c7a91e541421350753704595838c6c3f258f8e9a
Aimee Oz <aimee.oz.x@gmail.com>
2020-06-08 19:38:42 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -16,9 +16,10 @@ end
 
 def return_string_array(array)
   new_array = []
+  
   array.each do |element|
     string = element.to_s
-    new_array = new_array.push|string|
+    new_array = new_array.push(string)
   end
 
   puts new_array
