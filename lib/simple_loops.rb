be43b34b38eed17a4c467d18350e2adde22c4ef0
Aimee Oz <aimee.oz.x@gmail.com>
2020-06-08 19:34:24 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,19 @@
-# Write your methods here
+def loop_message_five_times(string)
+  5.times do
+    puts string
+  end
+end
+
+def loop_message_n_times(string, number)
+  number.times do
+    puts string
+  end
+end
+
+def output_array(array)
+  puts array  
+end
+
+def return_string_array(array)
+  array.to_s
+end
